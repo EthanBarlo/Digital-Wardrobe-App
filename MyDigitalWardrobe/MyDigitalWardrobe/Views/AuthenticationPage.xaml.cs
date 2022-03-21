666002e6b00f61b004281ad513037cd6d3ef4545
Serumz <65189904+Serumz@users.noreply.github.com>
2022-03-21 15:05:48 +0000

    Added NuGet Packages, And AuthPAge

---
+++ MyDigitalWardrobe/MyDigitalWardrobe/Views/AuthenticationPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyDigitalWardrobe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthenticationPage : ContentPage
+    {
+        public AuthenticationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
