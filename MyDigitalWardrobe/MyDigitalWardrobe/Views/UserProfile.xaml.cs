9e93524e7f138bf6f3ebd71bd13f46073618b077
Serumz <65189904+Serumz@users.noreply.github.com>
2022-03-21 15:50:18 +0000

    Added UserProfile Page

---
+++ MyDigitalWardrobe/MyDigitalWardrobe/Views/UserProfile.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyDigitalWardrobe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserProfile : ContentPage
+    {
+        private readonly string authKey = "AIzaSyB7wv7xyG148I_XUDv8OvNn-iQE1fjpbp4";
+        public UserProfile()
+        {
+            InitializeComponent();
+            //GetProfileUserInformationAndRefreshToken();
+        }
+
+        private async void logout_Clicked(object sender, EventArgs e)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(authKey));
+            try
+            {
+                var savedAuth = JsonConvert.DeserializeObject<Firebase.Auth.FirebaseAuth>(Preferences.Get("MyFirebaseAuthToken", "");
+                var refreshedContent = await authProvider.RefreshAuthAsync(savedAuth);
+                Preferences.Set("MyFirebaseRefreshToken", JsonConvert.SerializeObject(refreshedContent));
+                username.Text = savedAuth.User.Email;
+            }
+            catch (FirebaseAuthException ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", "Your login session has expired", "Ok");
+            }
+        }
+    }
+}
